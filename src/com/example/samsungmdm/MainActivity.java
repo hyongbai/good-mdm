69cbc9f52198a31713bcda0db5cba104f95c93fd
Wasiur Rahman <wrahman0@gmail.com>
2014-08-07 15:01:06 -0400

    Device admin fixed

--- src/com/example/samsungmdm/MainActivity.java
+++ src/com/example/samsungmdm/MainActivity.java
@@ -2,28 +2,26 @@ package com.example.samsungmdm;
 
 import android.app.Activity;
 import android.app.admin.DevicePolicyManager;
-import android.app.enterprise.EnterpriseDeviceManager;
-import android.app.enterprise.RestrictionPolicy;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
 	private Switch wifiSwitch;
 	private Switch bluetoothSwitch;
+    int REQUEST_ENABLE;
 
-	private EnterpriseDeviceManager edm;
-	private RestrictionPolicy restrictionPolicy;
+    DevicePolicyManager mDPM;
+    ComponentName mAdminName;
 
-	private DevicePolicyManager mDPM;
-	private ComponentName mAdminName;
-
-	@Override
+    @Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
@@ -31,55 +29,47 @@ public class MainActivity extends Activity {
 		wifiSwitch = (Switch) findViewById(R.id.wifiSwitch);
 		bluetoothSwitch = (Switch) findViewById(R.id.bluetoothSwitch);
 
+        mDPM = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
+        mAdminName = new ComponentName(this, MyDeviceAdminReceiver.class);
+
+        if (!mDPM.isAdminActive(mAdminName)){
+            //Not yet device admin
+            Intent intent = new Intent (DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+            intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, mAdminName);
+            intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION,"This needs to be added");
+            startActivityForResult(intent,REQUEST_ENABLE);
+        }else{
+            //Already a device admin
+            mDPM.lockNow();
+        }
+
 		wifiSwitch.setOnCheckedChangeListener(switchListeners);
 		bluetoothSwitch.setOnCheckedChangeListener(switchListeners);
 
-		edm = (EnterpriseDeviceManager) getSystemService(EnterpriseDeviceManager.ENTERPRISE_POLICY_SERVICE);
-		restrictionPolicy = edm.getRestrictionPolicy();
-
-		mDPM = (DevicePolicyManager)getSystemService(Context.DEVICE_POLICY_SERVICE);
-		mAdminName = new ComponentName(this, DeviceAdmin.class);
-
-		if (!mDPM.isAdminActive(mAdminName)) {
-			// try to become active – must happen here in this activity, to get result
-			Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
-			intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, mAdminName);
-			intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION, "Additional text explaining why this needs to be added.");
-//			startActivityForResult(intent, REQUEST_ENABLE);
-		} else {
-			// Already is a device administrator, can do security operations now.
-			mDPM.lockNow();
-		}
-
 	}
 
-	private OnCheckedChangeListener switchListeners = new OnCheckedChangeListener(){
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
+        if(REQUEST_ENABLE == requestCode){
+            if(resultCode==Activity.RESULT_OK){
+                //Has become the admin
+                Toast.makeText(getBaseContext(),"BECAME THE ADMIN",Toast.LENGTH_SHORT).show();
+            }else{
+                //failed to become the admin
+                Toast.makeText(getBaseContext(),"FAILED TO BECOME THE ADMIN",Toast.LENGTH_SHORT).show();
+                Log.e("ADMINISTRATION", "Request code is: " + requestCode + ", Result OK is: " + Activity.RESULT_OK);
+            }
+        }
+    }
 
-		@Override
-		public void onCheckedChanged(CompoundButton buttonView,
-				boolean isChecked) {
-			switch (buttonView.getId()){
-			case R.id.wifiSwitch:
-				if (!restrictionPolicy.isWiFiEnabled(false) && isChecked){
-					restrictionPolicy.setWiFiState(true);
-				}else if (restrictionPolicy.isWiFiEnabled(false) && !isChecked){
-					restrictionPolicy.setWiFiState(false);
-				}
-				break;
-			case R.id.bluetoothSwitch:
-				if (!restrictionPolicy.isBluetoothEnabled(false) && isChecked){
-					restrictionPolicy.setBluetoothState(true);
-				}else if (restrictionPolicy.isBluetoothEnabled(false) && !isChecked){
-					restrictionPolicy.setBluetoothState(false);
-				}
-				break;
-			}
+    private OnCheckedChangeListener switchListeners = new OnCheckedChangeListener(){
 
-		}
-
-	};
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView,
+                                     boolean isChecked) {
 
 
 
+        }
+    };
 }
--- src/com/example/samsungmdm/MyDeviceAdminReceiver.java
+++ src/com/example/samsungmdm/MyDeviceAdminReceiver.java
@@ -3,17 +3,19 @@ package com.example.samsungmdm;
 import android.app.admin.DeviceAdminReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.widget.Toast;
 
-public class DeviceAdmin extends DeviceAdminReceiver {
+/**
+ * Created by wasiur on 2014-08-07.
+ */
+public class MyDeviceAdminReceiver extends DeviceAdminReceiver {
 
     @Override
     public void onEnabled(Context context, Intent intent) {
-    	Toast.makeText(context, "Enabled",Toast.LENGTH_SHORT).show();
+        super.onEnabled(context, intent);
     }
 
     @Override
     public void onDisabled(Context context, Intent intent) {
-    	Toast.makeText(context, "Disabled",Toast.LENGTH_SHORT).show();
+        super.onDisabled(context, intent);
     }
 }
