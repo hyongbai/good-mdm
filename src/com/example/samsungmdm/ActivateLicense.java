13ca68bfd06354db2a49bcdc75967ad9f1ec9a7c
Dweep Shah <dweepcan1@msn.com>
2014-08-15 19:17:26 -0400

    Check for ELM and KLM Enrollment

--- src/com/example/samsungmdm/ActivateLicense.java
+++ src/com/example/samsungmdm/ActivateLicense.java
@@ -4,6 +4,7 @@ import android.app.enterprise.license.EnterpriseLicenseManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -17,11 +18,10 @@ public class ActivateLicense extends BroadcastReceiver {
     public static final String ELM_KEY = "92D4017683D1EE2644AF68C484DE6A1205DD41D435DBF5EEF7139CA1D8072BC56E7AD1D46855C034027624BC9AD04F82561E7F03A3F42D1723C301E57314933E";
     public static final String KLM_KEY = "KLM03-NMS7W-6PZZ0-ZV35D-1PSIN-2LZNW";
 
+    public static final String PREFS_NAME = "MyPrefsFile";
+
     private static final String SUCCESS = "success";
     private static final String FAILURE = "fail";
-
-    private static Boolean enrolled_ELM = false;
-    private static Boolean enrolled_KLM = false;
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -34,7 +34,8 @@ public class ActivateLicense extends BroadcastReceiver {
 
             if (SUCCESS.equals(status)) {
                 Log.d(MainActivity.TAG, "ELM success");
-                enrolled_ELM = true;
+
+                setPreferences(context.getApplicationContext(), "ELM_ENROLLED", true);
             } else if (FAILURE.equals(status)) {
                 final String errorMsg = "ELM failure: " + errorCode;
                 final String userMsg = "ELM Activation failed: " + errorCode;
@@ -58,7 +59,8 @@ public class ActivateLicense extends BroadcastReceiver {
 
             if (SUCCESS.equals(status)) {
                 Log.d(MainActivity.TAG, String.format("KLMS success: " + rc0 + " " + rc1 + " " + rc2));
-                enrolled_KLM = true;
+
+                setPreferences(context.getApplicationContext(), "KLM_ENROLLED", true);
             } else if (FAILURE.equals(status)) {
                 final String errorMsg = String.format("KLMS failure: " + rc0 + " " + rc1 + " " + rc2);
                 final String userMsg = String.format("KLM Activation failed: " + rc0);
@@ -74,7 +76,7 @@ public class ActivateLicense extends BroadcastReceiver {
     }
 
     public static synchronized void applyInitialLicenses(Context context) {
-        if(!(enrolled_ELM && enrolled_KLM)) {
+        if(!(getPreferences(context, "KLM_ENROLLED") && getPreferences(context, "ELM_ENROLLED"))) {
             Log.d(MainActivity.TAG, "Activating keys...");
             Log.d(MainActivity.TAG, String.format("ELM: %s", ELM_KEY));
             Log.d(MainActivity.TAG, String.format("KLM: %s", KLM_KEY));
@@ -84,5 +86,17 @@ public class ActivateLicense extends BroadcastReceiver {
             Log.d(MainActivity.TAG, "Already enrolled ELM and KLM keys");
             Toast.makeText(context.getApplicationContext(), "Already enrolled ELM and KLM keys", Toast.LENGTH_LONG).show();
         }
+    }
+
+    private static boolean getPreferences(Context context, String key) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(PREFS_NAME, 0);
+        return sharedPreferences.getBoolean(key, false);
+    }
+
+    private void setPreferences(Context context, String key, boolean value) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(PREFS_NAME, 0);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean(key, value);
+        editor.commit();
     }
 }
